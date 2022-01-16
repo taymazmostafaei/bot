819989d9bd58f3750daa79e86f57dd0402835831
taymazmostafaei <taymazmostafaei3@gmail.com>
2022-01-16 17:37:35 +0330

    Update model.php

--- src/server/model.php
+++ src/server/model.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "mysql";
+$username = "dbuser01";
+$password = "6RoZqQzz0il5LtSVMJhi80wjMR6g8NBORPEForH4";
 
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=taleghani", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=db01", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
